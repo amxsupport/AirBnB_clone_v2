88cb330a7928a8e6cfc6849467bc83813fac4994
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-12-17 23:49:07 +0100

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -40,3 +40,55 @@ class TestAmenity(unittest.TestCase):
             Session = sessionmaker(bind=cls.dbstorage._DBStorage__engine)
             cls.dbstorage._DBStorage__session = Session()
 
+    @classmethod
+    def tearDownClass(cls):
+        """Amenity testing teardown.
+        Restore original file.json.
+        Delete the FileStorage, DBStorage and Amenity test instances.
+        """
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+        del cls.amenity
+        del cls.filestorage
+        if type(models.storage) == DBStorage:
+            cls.dbstorage._DBStorage__session.close()
+            del cls.dbstorage
+
+    def test_pep8(self):
+        """Test pep8 styling."""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(["models/amenity.py"])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_docstrings(self):
+        """Check for docstrings."""
+        self.assertIsNotNone(Amenity.__doc__)
+
+    def test_attributes(self):
+        """Check for attributes."""
+        us = Amenity(email="a", password="a")
+        self.assertEqual(str, type(us.id))
+        self.assertEqual(datetime, type(us.created_at))
+        self.assertEqual(datetime, type(us.updated_at))
+        self.assertTrue(hasattr(us, "__tablename__"))
+        self.assertTrue(hasattr(us, "name"))
+        self.assertTrue(hasattr(us, "place_amenities"))
+
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_email_not_nullable(self):
+        """Test that email attribute is non-nullable."""
+        with self.assertRaises(OperationalError):
+            self.dbstorage._DBStorage__session.add(Amenity(password="a"))
+            self.dbstorage._DBStorage__session.commit()
+        self.dbstorage._DBStorage__session.rollback()
+        with self.assertRaises(OperationalError):
+            self.dbstorage._DBStorage__session.add(Amenity(email="a"))
+            self.dbstorage._DBStorage__session.commit()
+
