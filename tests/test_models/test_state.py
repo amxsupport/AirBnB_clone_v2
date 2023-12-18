d29a96d32c168678cc6c0518a35cfc2f119cad5b
amxsupport <itmasterdesigne@gmail.com>
2023-12-18 19:02:56 +0000

    update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -163,4 +163,18 @@ class TestState(unittest.TestCase):
         self.assertEqual(self.state.id, query[0][0])
         cursor.close()
 
+    def test_to_dict(self):
+        """Test to_dict method."""
+        state_dict = self.state.to_dict()
+        self.assertEqual(dict, type(state_dict))
+        self.assertEqual(self.state.id, state_dict["id"])
+        self.assertEqual("State", state_dict["__class__"])
+        self.assertEqual(self.state.created_at.isoformat(),
+                         state_dict["created_at"])
+        self.assertEqual(self.state.updated_at.isoformat(),
+                         state_dict["updated_at"])
+        self.assertEqual(self.state.name, state_dict["name"])
 
+
+if __name__ == "__main__":
+    unittest.main()
