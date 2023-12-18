99236c444a31ad47e11c87fe34717dbe5cb2afb3
amxsupport <itmasterdesigne@gmail.com>
2023-12-18 19:29:55 +0000

    update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -84,4 +84,38 @@ class TestUser(unittest.TestCase):
         self.assertTrue(hasattr(us, "places"))
         self.assertTrue(hasattr(us, "reviews"))
 
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_email_not_nullable(self):
+        """Test that email attribute is non-nullable."""
+        with self.assertRaises(OperationalError):
+            self.dbstorage._DBStorage__session.add(User(password="a"))
+            self.dbstorage._DBStorage__session.commit()
+        self.dbstorage._DBStorage__session.rollback()
+        with self.assertRaises(OperationalError):
+            self.dbstorage._DBStorage__session.add(User(email="a"))
+            self.dbstorage._DBStorage__session.commit()
+
+    def test_is_subclass(self):
+        """Check that User is a subclass of BaseModel."""
+        self.assertTrue(issubclass(User, BaseModel))
+
+    def test_init(self):
+        """Test initialization."""
+        self.assertIsInstance(self.user, User)
+
+    def test_two_models_are_unique(self):
+        """Test that different User instances are unique."""
+        us = User(email="a", password="a")
+        self.assertNotEqual(self.user.id, us.id)
+        self.assertLess(self.user.created_at, us.created_at)
+        self.assertLess(self.user.updated_at, us.updated_at)
+
+    def test_init_args_kwargs(self):
+        """Test initialization with args and kwargs."""
+        dt = datetime.utcnow()
+        st = User("1", id="5", created_at=dt.isoformat())
+        self.assertEqual(st.id, "5")
+        self.assertEqual(st.created_at, dt)
+
 
