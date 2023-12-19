3824dd9003ac6dc668f94b17aa36da85a98199ce
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-12-19 12:52:06 +0100

    Create db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines the DBStorage engine."""
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+
+class DBStorage:
+    """Represents a database storage engine.
+
+    Attributes:
+        __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
+        __session (sqlalchemy.Session): The working SQLAlchemy session.
+    """
+
+    __engine = None
+    __session = None
